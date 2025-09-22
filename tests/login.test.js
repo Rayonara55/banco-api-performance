dfcf2080e29f6c0449518711902130e06dffa8aa
rayonara <rayonaraazevedo@si.fiponline.edu.br>
2025-09-22 15:52:54 -0300

    add o arquivo inicial com teste de login

---
+++ tests/login.test.js
@@ -0,0 +1,34 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  iterations: 5,
+  thresholds:{
+      http_req_duration: ['p(90)<300']
+      //http_req_duration: ['p(90)<50', 'max<1'],
+      //http_req_failed: ['rate<0.01']
+
+  }
+};
+
+export default function () {
+  const url = 'http://localhost:8000/snap/informacoes/cpf';
+
+  const payload = JSON.stringify({
+    cpf: '10091128480',
+  });
+
+  const params = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+
+  const res = http.post(url, payload, params);
+ //o check n é p definir se o teste passou ou falhou, check são apenas p saber funcionamente sua api ta ok
+  check(res, {
+    'Validar que o status é 202': (r) => r.status === 202,
+  });
+
+  sleep(1);
+}
